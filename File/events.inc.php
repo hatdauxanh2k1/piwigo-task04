7c5c0ba57a2139fc28473a37282a60b3c5b7b2bb
Nong Manh Thang <84931087+hatdauxanh2k1@users.noreply.github.com>
2024-12-04 18:29:34 +0700

    Update events.inc.php

--- File/events.inc.php
+++ File/events.inc.php
@@ -345,7 +345,7 @@ function add_slick_slider_css()
 
 function add_slick_slider_js()
 {
-  echo '<script async src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>';
-  echo '<script  async src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js" integrity="sha512-HGOnQO9+SP1V92SrtZfjqxxtLmVzqZpjFFekvzZVWoiASSQgSr4cw9Kqd2+l8Llp4Gm0G8GIFJ4ddwZilcdb8A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>';
-  echo '<script async type="text/javascript" src="./plugins/ExtendedDescription/template/assets/slick-slider.js"></script>';
+  echo '<script defer src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>';
+  echo '<script  defer src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js" integrity="sha512-HGOnQO9+SP1V92SrtZfjqxxtLmVzqZpjFFekvzZVWoiASSQgSr4cw9Kqd2+l8Llp4Gm0G8GIFJ4ddwZilcdb8A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>';
+  echo '<script defer type="text/javascript" src="./plugins/ExtendedDescription/template/assets/slick-slider.js"></script>';
 }
